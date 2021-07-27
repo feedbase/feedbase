b0cccbc9c9b4df2bc12a6fad8a32667cdda3f38b
nikolai <mail@nikoli.fyi>
2021-07-27 14:16:29 -0400

    rm typename

--- pack.js
+++ pack.js
@@ -1,19 +1,17 @@
-module.exports = {
+{
   "objects": [
     {
-      name: "feedbase",
-      typename: "FeedbaseV1",
-      artifacts: "QmX6iabQB6YWSVLi2a8kATStaq68KbiEJZA1dycfQNk887",
-      addresses: {
+      "name": "feedbase",
+      "artifacts": "QmX6iabQB6YWSVLi2a8kATStaq68KbiEJZA1dycfQNk887",
+      "addresses": {
         "ropsten": "0xb901C647f4F8d5D4fA9FF23f95d91d6fC46C52Ff"
       }
     },
     {
-      name: "oracle_factory",
-      typename: "oracle_factory_v2",
-      artifacts: "QmTbbamu2NaURTNP6YkweuL9Qcsi4jwrRB5uhbhhyY1rmP",
-      addresses: {
-        ropsten: "0x8d0523bDF6F8e12Cf257557754d858649bB696fA"
+      "name": "oracle_factory",
+      "artifacts": "QmTbbamu2NaURTNP6YkweuL9Qcsi4jwrRB5uhbhhyY1rmP",
+      "addresses": {
+        "ropsten": "0x8d0523bDF6F8e12Cf257557754d858649bB696fA"
       }
     }
   ]
