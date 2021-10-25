59e26b88a388caac309c6179ee95555620124702
nikolai <mail@nikolai.fyi>
2021-10-24 23:44:28 -0400

    cleanup / no dpack

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,10 @@
 import path from 'path'
-import * as dpack from 'dpack'
 import * as sensor from './sensor'
 import { makeUpdateDigest } from './message'
 import * as format from './format'
 import * as autofeed from './autofeed'
 
-let dapp
-
-async function init () {
-  console.log('WARN hard coded packfile')
-  const packfile = path.join(__dirname, '../dpacks/feedbase.json')
-  dapp = await dpack.loadFromFile(packfile)
-}
-
 export {
-  init,
-  dapp,
   sensor,
   makeUpdateDigest,
   format,
