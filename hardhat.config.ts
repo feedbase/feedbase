956402401183c61458b33ea76f82ebc397027427
Nikolai Mushegian <924397+nmushegian@users.noreply.github.com>
2021-10-27 09:43:35 -0400

    test refactoring (#42)
    * helper refactoring (#40)
    
    * helper refactoring
    
    * refactoring
    
    * extract minihat
    
    * test refactoring
    
    * test refactoring
    
    * test refactoring

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,39 +1,9 @@
 import '@nomiclabs/hardhat-ethers'
 
 import './task/deploy-feedbase.ts'
-
-const { PRIVKEY, INFURA_PROJECT_ID } = process.env
-
-const privKey = PRIVKEY ?? Buffer.alloc(32).toString('hex')
-
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 export default {
-  solidity: '0.8.9',
-  defaultNetwork: 'hardhat',
-  networks: {
-    hardhat: {},
-    mainnet: {
-      url: `https://mainnet.infura.io/v3/${INFURA_PROJECT_ID}`,
-      chainId: 1,
-      accounts: [`0x${privKey}`]
-    },
-    kovan: {
-      url: `https://kovan.infura.io/v3/${INFURA_PROJECT_ID}`,
-      chainId: 42,
-      accounts: [`0x${privKey}`]
-    },
-    ropsten: {
-      url: `https://ropsten.infura.io/v3/${INFURA_PROJECT_ID}`,
-      chainId: 3,
-      accounts: [`0x${privKey}`]
-    },
-    manaflow: {
-      url: 'http://localhost:8545',
-      chainId: 1,
-      accounts: [`0x${privKey}`]
-
-    }
-  }
+  solidity: '0.8.9'
 }
