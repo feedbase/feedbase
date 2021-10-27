06842cd2bbe086c9e82bf27083320cdf385e6852
nikolai <mail@nikolai.fyi>
2021-10-27 09:34:37 -0400

    test refactoring

--- test/feedbase-test.ts
+++ test/feedbase-test.ts
@@ -12,7 +12,6 @@ import { makeUpdateDigest } from '../src'
 
 const debug = require('debug')('feedbase:test')
 
-
 let cash
 let fb
 let signers
@@ -24,7 +23,6 @@ const use = (n) => {
   cash = cash.connect(signer)
   fb = fb.connect(signer)
 }
-
 
 describe('feedbase', () => {
   const UINT_MAX = Buffer.from('ff'.repeat(32), 'hex')
@@ -55,47 +53,6 @@ describe('feedbase', () => {
     debug(tag, seq, sec, ttl, val)
   })
              
-  it('oracle relay', async function () {
-    const { chainId } = network.config
-    debug('chainId: ', chainId)
-
-    const FeedbaseFactory = await ethers.getContractFactory('Feedbase')
-    const fb = await FeedbaseFactory.deploy()
-    const BasicReceiverFactoryFactory = await ethers.getContractFactory('BasicReceiverFactory')
-    const factory = await BasicReceiverFactoryFactory.deploy(fb.address)
-
-    const res = await send(factory.build);
-    const oracleAddr = res.events[0].args[0]
-
-    const oracle = await new ethers.Contract(oracleAddr, BasicReceiver.abi, ali)
-    await send(oracle.setSigner, ALI, 1000000000000)
-
-    const sttl = await oracle.signerTTL(ALI)
-    debug(`sttl: ${sttl}`)
-
-    const oracleChainId = await oracle.chainId()
-    debug(`chainId: ${chainId}, type ${typeof (chainId)}`)
-    debug(`oracleChainId: ${oracleChainId}, type ${typeof (oracleChainId)}`)
-    want(chainId).equal(oracleChainId.toNumber())
-
-    const digest = makeUpdateDigest({
-      tag,
-      val: val,
-      seq: seq,
-      sec: sec,
-      ttl: ttl,
-      chainId: chainId,
-      receiver: oracle.address
-    })
-    debug(`digest: ${Buffer.from(digest).toString('hex')}`)
-
-    const signature = await ali.signMessage(digest)
-    debug(`signature ${signature}`)
-    const sig = ethers.utils.splitSignature(signature)
-    // debug(sig);
-    await send(oracle.submit, tag, seq, sec, ttl, val, '0'.repeat(40), sig.v, sig.r, sig.s)
-  })
-
   it('ttl on read', async function () {
 
     const FeedbaseFactory = await ethers.getContractFactory('Feedbase')
@@ -117,171 +74,81 @@ describe('feedbase', () => {
   })
 
 
-  //TODO: auth tests
-  describe('receiver setSigner factory build', () => {
-    let oracle, chainId;
-    beforeEach(async () => {
-      const BasicReceiverFactoryFactory = await ethers.getContractFactory('BasicReceiverFactory')
-      const factory = await BasicReceiverFactoryFactory.deploy(fb.address)
+  describe('cost setCost', () => {
 
-      const tx = await factory.build()
-      // debug('create', tx)
-      const res = await tx.wait()
-      const oracleAddr = res.events[0].args[0]
+    describe('balance zero', () => {
+      it('cost too high', async function () {
+        const cost = 1
+        const setCost = await fb.setCost(tag, cash.address, cost)
+        fail('VM Exception while processing transaction: reverted with panic code 0x11 (Arithmetic operation underflowed or overflowed outside of an unchecked block)',
+          fb.push, tag, val, ttl, cash.address)
+      })
 
-      oracle = await new ethers.Contract(oracleAddr, BasicReceiver.abi, ali)
-      await oracle.setSigner(ALI, 1000000000000)
-
-      chainId = network.config.chainId;
+      it('cost ok', async function () {
+        const cost = 0
+        const setCost = await fb.setCost(tag, cash.address, cost)
+        await fb.push(tag, val, ttl, cash.address);
+      })
     })
 
-    //sequence number must increase
-    it('seq # setCost deposit request submit', async function () {
-      const cost     = 10;
-      const relayFee = 11;
-
-      const setCost = await oracle.setCost(tag, cash.address, cost);
-      const deposit = await fb.deposit(cash.address, ALI, cost * 2, {value: cost * 2});
-      const request = await fb.request(oracle.address, tag, cash.address, cost * 2);
-
-      const digest = makeUpdateDigest({
-        tag,
-        val: val,
-        seq: seq,
-        sec: sec,
-        ttl: ttl,
-        chainId: chainId,
-        receiver: oracle.address
-      })
-      debug(`digest: ${Buffer.from(digest).toString('hex')}`)
-
-      const signature = await ali.signMessage(digest)
-      debug(`signature ${signature}`)
-      const sig = ethers.utils.splitSignature(signature)
-      // debug(sig);
-
-      await oracle.setRelayFee(tag, cash.address, relayFee);
-
-      //submit twice with same seq
-      await oracle.submit(tag, seq, sec, ttl, val, cash.address, sig.v, sig.r, sig.s);
-      await fail('submit-seq', oracle.submit, tag, seq, sec, ttl, val, cash.address, sig.v, sig.r, sig.s);
-    });
-
-    //TODO: cost > relay fee
-    it('collect (cost < relay fee)', async function () {
-      const cost     = 10;
-      const relayFee = 11;
-
-      const setCost = await oracle.setCost(tag, cash.address, cost);
-      const deposit = await fb.deposit(cash.address, ALI, cost * 2);
-      const request = await fb.request(oracle.address, tag, cash.address, cost * 2);
-      await oracle.setRelayFee(tag, cash.address, relayFee);
-
-      for( let i = 0; i < 2; i++ ) {
-        const digest = makeUpdateDigest({
-          tag,
-          val: val,
-          seq: seq + i,
-          sec: sec,
-          ttl: ttl,
-          chainId: chainId,
-          receiver: oracle.address
-        })
-        debug(`digest: ${Buffer.from(digest).toString('hex')}`)
-
-        const signature = await ali.signMessage(digest)
-        debug(`signature ${signature}`)
-        const sig = ethers.utils.splitSignature(signature)
-        // debug(sig);
-        await oracle.submit(tag, seq + i, sec, ttl, val, cash.address, sig.v, sig.r, sig.s);
-      }
-
-      const bal     = await cash.balanceOf(ALI);
-      const collect = await oracle.collect(cash.address, ALI);
-      await collect.wait()
-
-      want(await cash.balanceOf(ALI)).to.eql(bal.add(cost * 2));
-    });
-  })
-
-  describe('feedbase', () => {
-
-    describe('cost setCost', () => {
-
-      describe('balance zero', () => {
-        it('cost too high', async function () {
-          const cost = 1
-          const setCost = await fb.setCost(tag, cash.address, cost)
-          fail('VM Exception while processing transaction: reverted with panic code 0x11 (Arithmetic operation underflowed or overflowed outside of an unchecked block)',
-            fb.push, tag, val, ttl, cash.address)
-        })
-
-        it('cost ok', async function () {
-          const cost = 0
-          const setCost = await fb.setCost(tag, cash.address, cost)
-          await fb.push(tag, val, ttl, cash.address);
-        })
+    describe('push balance nonzero', () => {
+      let bal;
+      beforeEach(async () => {
+        bal = 1000;
       })
 
-      describe('push balance nonzero', () => {
-        let bal;
-        beforeEach(async () => {
-          bal = 1000;
-        })
-
-        it('cost too high', async function () {
-          const cost = 1001
-          const setCost = await fb.setCost(tag, cash.address, cost)
-          const deposit = await fb.deposit(cash.address, ALI, bal, {value: bal});
-          const request = await fb.request(ALI, tag, cash.address, bal);
-          await fail('VM Exception while processing transaction: reverted with panic code 0x11 (Arithmetic operation underflowed or overflowed outside of an unchecked block)',
-            fb.push, tag, val, ttl, cash.address)
-        })
-
-        it('cost ok', async function () {
-          const cost = 1000
-          const setCost = await fb.setCost(tag, cash.address, cost)
-          const deposit = await fb.deposit(cash.address, ALI, bal, {value: bal});
-          const request = await fb.request(ALI, tag, cash.address, bal);
-          await fb.push(tag, val, ttl, cash.address);
-        })
-      })
-      describe('deposit', () => {
-        let bal;
-        beforeEach(async () => {
-          bal      = await cash.balanceOf(ALI);
-        })
-        it('zero', async function () {
-          const amt      = 0;
-          const deposit  = await fb.deposit(cash.address, ALI, amt, {value: amt});
-          want(await cash.balanceOf(ALI)).to.eql(bal.sub(amt));
-        })
-        it('nonzero', async function () {
-          const amt      = 3;
-          const deposit  = await fb.deposit(cash.address, ALI, amt, {value: amt});
-          want(await cash.balanceOf(ALI)).to.eql(bal.sub(amt));
-        })
+      it('cost too high', async function () {
+        const cost = 1001
+        const setCost = await fb.setCost(tag, cash.address, cost)
+        const deposit = await fb.deposit(cash.address, ALI, bal, {value: bal});
+        const request = await fb.request(ALI, tag, cash.address, bal);
+        await fail('VM Exception while processing transaction: reverted with panic code 0x11 (Arithmetic operation underflowed or overflowed outside of an unchecked block)',
+          fb.push, tag, val, ttl, cash.address)
       })
 
-      describe('withdraw', () => {
-        it('zero', async function () {
-          const amt      = 0;
-          const bal      = await cash.balanceOf(ALI);
-          const withdraw = await fb.withdraw(cash.address, ALI, amt);
-          want(await cash.balanceOf(ALI)).to.eql(bal.add(amt));
-        })
-        it('nonzero', async function () {
-          const amt      = 3;
-          const deposit  = await fb.deposit(cash.address, ALI, amt, {value: amt});
-          const bal      = await cash.balanceOf(ALI);
-          const withdraw = await fb.withdraw(cash.address, ALI, amt);
-          want(await cash.balanceOf(ALI)).to.eql(bal.add(amt));
-        })
-        it('underflow', async function () {
-          const amt      = 3;
-          const deposit  = await fb.deposit(cash.address, ALI, amt, {value: amt});
-          await fail('underflow', fb.withdraw, cash.address, ALI, amt+1);
-        })
+      it('cost ok', async function () {
+        const cost = 1000
+        const setCost = await fb.setCost(tag, cash.address, cost)
+        const deposit = await fb.deposit(cash.address, ALI, bal, {value: bal});
+        const request = await fb.request(ALI, tag, cash.address, bal);
+        await fb.push(tag, val, ttl, cash.address);
+      })
+    })
+    describe('deposit', () => {
+      let bal;
+      beforeEach(async () => {
+        bal      = await cash.balanceOf(ALI);
+      })
+      it('zero', async function () {
+        const amt      = 0;
+        const deposit  = await fb.deposit(cash.address, ALI, amt, {value: amt});
+        want(await cash.balanceOf(ALI)).to.eql(bal.sub(amt));
+      })
+      it('nonzero', async function () {
+        const amt      = 3;
+        const deposit  = await fb.deposit(cash.address, ALI, amt, {value: amt});
+        want(await cash.balanceOf(ALI)).to.eql(bal.sub(amt));
+      })
+    })
+
+    describe('withdraw', () => {
+      it('zero', async function () {
+        const amt      = 0;
+        const bal      = await cash.balanceOf(ALI);
+        const withdraw = await fb.withdraw(cash.address, ALI, amt);
+        want(await cash.balanceOf(ALI)).to.eql(bal.add(amt));
+      })
+      it('nonzero', async function () {
+        const amt      = 3;
+        const deposit  = await fb.deposit(cash.address, ALI, amt, {value: amt});
+        const bal      = await cash.balanceOf(ALI);
+        const withdraw = await fb.withdraw(cash.address, ALI, amt);
+        want(await cash.balanceOf(ALI)).to.eql(bal.add(amt));
+      })
+      it('underflow', async function () {
+        const amt      = 3;
+        const deposit  = await fb.deposit(cash.address, ALI, amt, {value: amt});
+        await fail('underflow', fb.withdraw, cash.address, ALI, amt+1);
       })
     })
   })
---
+++ test/receiver-test.ts
@@ -0,0 +1,130 @@
+import Feedbase from '../artifacts/contracts/Feedbase.sol/Feedbase.json'
+import BasicReceiverFactory from '../artifacts/contracts/Receiver.sol/BasicReceiverFactory.json'
+import BasicReceiver from '../artifacts/contracts/Receiver.sol/BasicReceiver.json'
+
+import { makeUpdateDigest } from '../src'
+
+import * as hh from 'hardhat'
+import { ethers } from 'hardhat'
+import { want, send, fail, snapshot, revert, U256_MAX } from 'minihat'
+
+const debug = require('debug')('feedbase:test')
+
+describe('receiver BasicReceiver BasicReceiverFactory', ()=>{
+  let ali, bob;
+  let ALI, BOB;
+  let fb, fb_type;
+  let rec, rec_type;
+  let recfab, recfab_type;
+  let cash,  cash_type;
+
+  let tag, seq, sec, ttl, val;
+  let chainId;
+
+  before(async () => {
+    [ali, bob] = await ethers.getSigners();
+    [ALI, BOB] = [ali.address, bob.address];
+
+    fb_type = await ethers.getContractFactory('Feedbase');
+    fb = await fb_type.deploy();
+
+    rec_type = await ethers.getContractFactory('BasicReceiver');
+    rec = await rec_type.deploy(fb.address);
+
+    cash_type = await ethers.getContractFactory('MockToken');
+    cash = await cash_type.deploy('CASH', 'CASH');
+
+    await send(cash.mint, ALI, 1000)
+    await send(cash.approve, fb.address, U256_MAX)
+
+    await send(rec.setSigner, ALI, 1000000000000)
+
+    await snapshot(hh);
+  });
+  beforeEach(async () => {
+    await revert(hh)
+    tag = Buffer.from('USDCASH'.padStart(32, '\0'))
+    seq = 1
+    sec = Math.floor(Date.now() / 1000)
+    ttl = 10000000000000
+    val = Buffer.from('11'.repeat(32), 'hex')
+    chainId = hh.network.config.chainId;
+  })
+
+  describe('eip712 submit', ()=>{
+    it('pass submit', async () => {
+      const recChainId = await rec.chainId()
+      want(chainId).equal(recChainId.toNumber())
+
+      const digest = makeUpdateDigest({
+        tag, val, seq, sec, ttl,
+        chainId: chainId,
+        receiver: rec.address
+      })
+
+      const signature = await ali.signMessage(digest)
+      const sig = ethers.utils.splitSignature(signature)
+      await send(rec.submit, tag, seq, sec, ttl, val, '0'.repeat(40), sig.v, sig.r, sig.s)
+    });
+
+    //sequence number must increase
+    it('seq must increase / setCost deposit request submit', async function () {
+      const cost     = 10;
+      const relayFee = 11;
+
+      const setCost = await rec.setCost(tag, cash.address, cost);
+      const deposit = await fb.deposit(cash.address, ALI, cost * 2, {value: cost * 2});
+      const request = await fb.request(rec.address, tag, cash.address, cost * 2);
+
+      const digest = makeUpdateDigest({
+        tag, val, seq, sec, ttl,
+        chainId: chainId,
+        receiver: rec.address
+      })
+      const signature = await ali.signMessage(digest)
+      const sig = ethers.utils.splitSignature(signature)
+
+      await rec.setRelayFee(tag, cash.address, relayFee);
+
+      //submit twice with same seq
+      await rec.submit(tag, seq, sec, ttl, val, cash.address, sig.v, sig.r, sig.s);
+      await fail('submit-seq', rec.submit, tag, seq, sec, ttl, val, cash.address, sig.v, sig.r, sig.s);
+    });
+
+    //TODO: cost > relay fee
+    it('collect (cost < relay fee)', async function () {
+      const cost     = 10;
+      const relayFee = 11;
+
+      const setCost = await rec.setCost(tag, cash.address, cost);
+      const deposit = await fb.deposit(cash.address, ALI, cost * 2);
+      const request = await fb.request(rec.address, tag, cash.address, cost * 2);
+      await rec.setRelayFee(tag, cash.address, relayFee);
+
+      for( let i = 0; i < 2; i++ ) {
+        const digest = makeUpdateDigest({
+          tag,
+          val: val,
+          seq: seq + i,
+          sec: sec,
+          ttl: ttl,
+          chainId: chainId,
+          receiver: rec.address
+        })
+
+        const signature = await ali.signMessage(digest)
+        const sig = ethers.utils.splitSignature(signature)
+        await rec.submit(tag, seq + i, sec, ttl, val, cash.address, sig.v, sig.r, sig.s);
+      }
+
+      const bal     = await cash.balanceOf(ALI);
+      const collect = await rec.collect(cash.address, ALI);
+      await collect.wait()
+
+      want(await cash.balanceOf(ALI)).to.eql(bal.add(cost * 2));
+    });
+
+  })
+
+
+});
