a1fd41f9a976b1d0279143bde73ae08941b460d7
Grant Steuart <gmsteuart@gmail.com>
2021-10-28 13:29:26 -0500

    feat: initial medianizer

--- contracts/combinators/Medianizer.sol
+++ contracts/combinators/Medianizer.sol
@@ -25,14 +25,39 @@ contract MedianizerCombinator {
 
   function push(bytes32 tag) public {
     (uint256 quorum, address[] memory sources) = gov.getSelectors();
-    require(sources.length % 2 != 0, 'ERR_SOURCE_COUNT');
+    bytes32[] memory data = new bytes32[](sources.length);
     uint256 minttl = type(uint256).max;
-    uint256 median = 0;
-    for(uint i = 0; i < sources.length; i++ ) {
+    uint256 count = 0;
+  
+    for(uint256 i = 1; i < sources.length; i++) {
       (bytes32 val, uint256 ttl) = fb.read(sources[i], tag);
-      // TODO: actual median implementation
-      median += uint256(val);
+      if (ttl < block.timestamp) {
+        continue;
+      }
+      if (count == 0 || val >= data[count - 1]) {
+        data[count] = val;
+      } else {
+        uint256 j = 0;
+        while (val >= data[j]) {
+          j++;
+        }
+        for (uint256 k = count; k > j; k--) {
+          data[k] = data[k - 1];
+        }
+        data[j] = val;
+      }
+      count++;
     }
-    fb.push(tag, bytes32(median), minttl, address(0));
+
+      bytes32 median;
+      if (count % 2 == 0) {
+        uint256 val1 = uint256(data[(count / 2) - 1]);
+        uint256 val2 = uint256(data[count / 2]);
+        median = bytes32((val1 + val2) / 2);
+      } else {
+        median = data[(count - 1) / 2];
+      }
+
+      fb.push(tag, median, minttl, address(0));
   }
 }
--- test/combinators/medianizer-test.ts
+++ test/combinators/medianizer-test.ts
@@ -1,5 +1,3 @@
-import Feedbase from '../artifacts/contracts/Feedbase.sol/Feedbase.json'
-import Token from '../artifacts/contracts/erc20/MockToken.sol/MockToken.json'
 import { ethers } from 'hardhat'
 import { Contract } from 'hardhat/types'
 import { want } from 'minihat'
@@ -7,66 +5,48 @@ import { want } from 'minihat'
 const debug = require('debug')('feedbase:test')
 const { constants, BigNumber, utils } = ethers
 const { MaxUint256 } = constants
-const { formatBytes32String, parseEther } = utils
+const { formatBytes32String, parseEther, parseBytes32String } = utils
 
 const toAddressList = (xs: Contract[]): string[] => xs.map(x => x.address)
 
 describe('medianizer', () => {
   let cash, sources
-  let feedbase, medianizer, selector
+  let fb, medianizer, selector
   let ali, bob
   let s1, s2, s3
 
   const tag = formatBytes32String('MEDCASH')
 
   before(async () => {
-    [ali, bob] = await ethers.getSigners()
+    [ali, bob, s1, s2, s3] = await ethers.getSigners()
 
     const FeedbaseFactory = await ethers.getContractFactory('Feedbase')
-    feedbase = await FeedbaseFactory.deploy()
+    fb = await FeedbaseFactory.deploy()
 
     const FixedSelectorProviderFactory = await ethers.getContractFactory('FixedSelectorProvider')
     selector = await FixedSelectorProviderFactory.deploy()
 
     const MedianizerFactory = await ethers.getContractFactory('MedianizerCombinator')
-    medianizer = await MedianizerFactory.deploy(selector.address, feedbase.address)
+    medianizer = await MedianizerFactory.deploy(selector.address, fb.address)
 
     const TokenDeployer = await ethers.getContractFactory('MockToken')
     cash = await TokenDeployer.deploy('CASH', 'CASH')
 
     await cash.mint(ali.address, 10000)
-    await cash.approve(feedbase.address, MaxUint256)
+    await cash.approve(fb.address, MaxUint256)
 
-    const aliCash = await cash.balanceOf(ali.address)
-
-    s1 = await TokenDeployer.deploy('SRC1', 'SRC1')
-    s2 = await TokenDeployer.deploy('SRC2', 'SRC2')
-    s3 = await TokenDeployer.deploy('SRC3', 'SRC3')
     sources = [s1, s2, s3]
-
-    debug(`feedbase deployed at: ${feedbase.address}`)
-    debug(`selector deployed at: ${selector.address}`)
-    debug(`medianizer deployed at: ${medianizer.address}`)
-    debug(`cash deployed at: ${cash.address}`)
-    debug(`ali: ${ali.address}`)
-    debug(`ali cash: ${aliCash}`)
-    debug(`s1: ${s1.address}`)
   })
 
   it('basics', async function () {
     const fee = 5
     const amt = 1000
 
-    await feedbase.setCost(tag, cash.address, fee)
+    await fb.setCost(tag, cash.address, fee)
     // Provide cash for combinator requests
-    await feedbase.deposit(cash.address, ali.address, amt, { value: parseEther('0.1') })
-    const after = await cash.balanceOf(feedbase.address)
-    debug(`cash bal after: ${after}`)
-    const bal = await feedbase.balances(cash.address, ali.address)
-    const medianizerCash = await feedbase.balances(cash.address, medianizer.address)
+    await fb.deposit(cash.address, ali.address, amt, { value: parseEther('0.1') })
+    const bal = await fb.balances(cash.address, ali.address)
     want(bal.toNumber()).to.eql(amt)
-    debug(`ali cash bal: ${bal}`)
-    debug(`medianizer cash bal: ${medianizerCash}`)
   })
 
   it('selector', async function () {
@@ -76,30 +56,34 @@ describe('medianizer', () => {
     await selector.setSelectors(toAddressList(sources))
     const { set } = await selector.getSelectors()
     want(set).to.eql(toAddressList(sources))
-    debug('sources: ', set)
   })
 
   it('poke', async function () {
     const amt = 1000
     const src = medianizer.address
-    const predeposit = await feedbase.balances(cash.address, ali.address)
-    await feedbase.deposit(cash.address, ali.address, amt)
-    const postdeposit = await feedbase.balances(cash.address, ali.address)
+    const predeposit = await fb.balances(cash.address, ali.address)
+    await fb.deposit(cash.address, ali.address, amt)
+    const postdeposit = await fb.balances(cash.address, ali.address)
     want(predeposit.toNumber() + amt).to.eql(postdeposit.toNumber())
-    await feedbase.request(src, tag, cash.address, amt)
-    const paid = await feedbase.requested(src, tag, cash.address)
+    await fb.request(src, tag, cash.address, amt)
+    const paid = await fb.requested(src, tag, cash.address)
     want(paid.toNumber()).to.eql(amt)
-    await feedbase.deposit(cash.address, medianizer.address, amt)
+    await fb.deposit(cash.address, medianizer.address, amt)
     await medianizer.poke(tag, cash.address)
-    const paidReqs = await Promise.all(sources.map(async s => await feedbase.requested(s.address, tag, cash.address)))
+    const paidReqs = await Promise.all(sources.map(async s => await fb.requested(s.address, tag, cash.address)))
     want(paidReqs.map((x: typeof BigNumber) => x.toNumber())).to.eql([333, 333, 333])
   })
 
   it('push', async function () {
-    const vals = [11, 12, 12]
+    const vals = [1000, 1200, 1200].map(x => formatBytes32String(`${x}`))
     const ttl = 10 * 10 ** 12
-    // sources.map(async (s, idx) => {
-    //   await feedbase.push(tag, vals[idx], ttl, cash.address)
-    // })
+    await Promise.all(sources.map(async (src, idx) => {
+      const con = fb.connect(src)
+      await con.push(tag, vals[idx], ttl, cash.address)
+    }))
+
+    await medianizer.push(tag)
+    const [median] = await fb.read(medianizer.address, tag)
+    want(parseBytes32String(median)).to.eql('1200')
   })
 })
