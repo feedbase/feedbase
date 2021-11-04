bd657acee873ca842100348c1e4b72c57668e116
nikolai <mail@nikolai.fyi>
2021-11-04 07:54:43 -0400

    consistent repo layouts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,5 +5,8 @@ import './task/deploy-feedbase.ts'
  * @type import('hardhat/config').HardhatUserConfig
  */
 export default {
-  solidity: '0.8.9'
+  solidity: '0.8.9',
+  paths: {
+    sources: "sol"
+  }
 }
