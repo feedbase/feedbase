efbe090fdae6103d204d0d535c66b021efaa75f8
nikolai <mail@nikolai.fyi>
2021-11-04 08:06:30 -0400

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-import path from 'path'
 import * as sensor from './sensor'
-import { makeUpdateDigest } from './message'
 import * as format from './format'
-import * as autofeed from './autofeed'
+import { autofeed } from './autofeed'
+import { makeUpdateDigest } from './message'
 
 export {
   sensor,
