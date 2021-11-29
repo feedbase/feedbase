a57740e0e39a7dfc6c124348a13ea16af3c0c61a
Kevin Bravo <kbravo@protonmail.com>
2021-11-29 11:14:10 -0500

    delete MockAggregator and MockRegistry

--- sol/mocks/MockAggregator.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.7.0;
-//import "@chainlink/contracts/src/v0.7/interfaces/AggregatorV2V3Interface.sol";
-import "@chainlink/contracts/src/v0.7/tests/MockV3Aggregator.sol";
-
-contract MockAggregator is MockV3Aggregator {
-  constructor(uint8 _decimals, int256 _initialAnswer) MockV3Aggregator(_decimals, _initialAnswer) {}
-}
--- sol/mocks/MockRegistry.sol
+++
@@ -1,5 +0,0 @@
-pragma solidity ^0.7.0;
-pragma abicoder v2;
-
-import "chainlink-feed-registry/contracts/FeedRegistry.sol";
-contract MockRegistry is FeedRegistry {}
