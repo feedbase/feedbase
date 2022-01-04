b5c5cc7a501d470eabf392e041ceb25a1a2010cf
nikolai <mail@nikolai.fyi>
2022-01-04 08:21:15 -0400

    deploy task

--- task/deploy-feedbase.ts
+++ task/deploy-feedbase.ts
@@ -1,7 +1,9 @@
+const debug = require('debug')('feedbase:task')
+
 import { task } from 'hardhat/config'
 import { HardhatRuntimeEnvironment, TaskArguments } from 'hardhat/types'
 
-const debug = require('debug')('feedbase:task')
+import { PackBuilder } from 'dpack'
 
 task('deploy-feedbase', 'deploy Feedbase and BasicReceiverFactory')
   .setAction(async (args: TaskArguments, hre: HardhatRuntimeEnvironment) => {
@@ -10,20 +12,42 @@ task('deploy-feedbase', 'deploy Feedbase and BasicReceiverFactory')
     const [acct] = await hre.ethers.getSigners()
     const deployer = acct.address
 
-    console.log(`Deploying contracts using ${deployer} to ${network.name}`)
+    debug(`Deploying contracts using ${deployer} to ${network.name}`)
 
     const FeedbaseDeployer = await hre.ethers.getContractFactory('Feedbase')
     const fb = await FeedbaseDeployer.deploy()
     await fb.deployed()
-    console.log('Feedbase deployed to : ', fb.address)
+    debug('Feedbase deployed to : ', fb.address)
     const FeedbaseArtifact = await hre.artifacts.readArtifact('Feedbase')
 
+    const pb = new PackBuilder(network.name);
+    await pb.packObject({
+      objectname: 'feedbase',
+      address: fb.address,
+      typename: 'Feedbase',
+      artifact: FeedbaseArtifact
+    });
+    await pb.packType({
+      typename: 'Feedbase',
+      artifact: FeedbaseArtifact
+    });
+    
+    const pack = await pb.build();
+    console.log(JSON.stringify(pack, null, 2))
+
+    return pack
+  })
+
+/*
+task('deploy-receiver-factory', 'deploy BasicReceiverFactory')
+  .addParam('feedbaseAddress', 'feedbase address')
+  .setAction(async (args: TaskArguments, hre: HardhatRuntimeEnvironment) => {
     const BasicReceiverFactoryDeployer = await hre.ethers.getContractFactory('BasicReceiverFactory')
-    const brf = await BasicReceiverFactoryDeployer.deploy(fb.address)
+    const brf = await BasicReceiverFactoryDeployer.deploy(args.feedbaseAddress)
     await brf.deployed()
     console.log('BasicReceiverFactory deployed to : ', brf.address)
 
     const BasicReceiverArtifact = await hre.artifacts.readArtifact('BasicReceiver')
     const BasicReceiverFactoryArtifact = await hre.artifacts.readArtifact('BasicReceiverFactory')
-
   })
+*/
