f3de68b5050ab054c7d5bcda5272bac434f35932
booklearner <booklearner@proton.me>
2022-08-26 17:59:27 -0400

    add tasks to deploy receiver factory and  medianizer

--- task/deploy-feedbase.ts
+++ task/deploy-feedbase.ts
@@ -5,52 +5,59 @@ const debug = require('debug')('feedbase:task')
 const dpack = require('@etherpacks/dpack')
 
 task('deploy-feedbase', 'deploy Feedbase')
-  .addFlag('stdout', 'print the dpack to stdout')
-  .addOptionalParam('outfile', 'save the dpack to this path')
-  .setAction(async (args: TaskArguments, hre: any) => {
-    const { ethers, network } = hre
+    .addFlag('stdout', 'print the dpack to stdout')
+    .addFlag('pack', 'whether or not to save the dpack')
+    .addOptionalParam('outfile', 'save the dpack to this path')
+    .setAction(async (args: TaskArguments, hre: any) => {
+        const { ethers, network } = hre
 
-    const [acct] = await hre.ethers.getSigners()
-    const deployer = acct.address
+        const [acct] = await hre.ethers.getSigners()
+        const deployer = acct.address
 
-    debug(`Deploying contracts using ${deployer} to ${network.name}`)
+        console.log(`Deploying contracts using ${deployer} to ${network.name}`)
 
-    const FeedbaseArtifact = require('../artifacts/src/Feedbase.sol/Feedbase.json')
-    debug('Loaded artifact')
-    const FeedbaseDeployer = ethers.ContractFactory.fromSolidity(FeedbaseArtifact, acct)
-    const fb = await FeedbaseDeployer.deploy()
-    await fb.deployed()
-    debug('Feedbase deployed to : ', fb.address)
+        const FeedbaseArtifact = require('../artifacts/src/Feedbase.sol/Feedbase.json')
+        const FeedbaseDeployer = ethers.ContractFactory.fromSolidity(FeedbaseArtifact, acct)
+        const fb = await FeedbaseDeployer.deploy()
+        await fb.deployed()
+        console.log('Feedbase deployed to : ', fb.address)
 
-    const pb = new dpack.PackBuilder(network.name)
-    await pb.packObject({
-      objectname: 'feedbase',
-      address: fb.address,
-      typename: 'Feedbase',
-      artifact: FeedbaseArtifact
-    }, true) // alsoPackType
+        if (args.pack) {
+            const pb = new dpack.PackBuilder(network.name)
+            await pb.packObject({
+                objectname: 'feedbase',
+                address: fb.address,
+                typename: 'Feedbase',
+                artifact: FeedbaseArtifact
+            }, true) // alsoPackType
 
-    const pack = await pb.build()
-    const str = JSON.stringify(pack, null, 2)
-    if (args.stdout) {
-      console.log(str)
-    }
-    if (args.outfile) {
-      require('fs').writeFileSync(args.outfile, str)
-    }
-    return pack
-  })
+            const pack = await pb.build()
+            const str = JSON.stringify(pack, null, 2)
+            if (args.stdout) {
+                console.log(str)
+            }
+            if (args.outfile) {
+                require('fs').writeFileSync(args.outfile, str)
+            }
+            return pack
+        }
+    })
 
-/*
 task('deploy-receiver-factory', 'deploy BasicReceiverFactory')
-  .addParam('feedbaseAddress', 'feedbase address')
-  .setAction(async (args: TaskArguments, hre: HardhatRuntimeEnvironment) => {
-    const BasicReceiverFactoryDeployer = await hre.ethers.getContractFactory('BasicReceiverFactory')
-    const brf = await BasicReceiverFactoryDeployer.deploy(args.feedbaseAddress)
-    await brf.deployed()
-    console.log('BasicReceiverFactory deployed to : ', brf.address)
+    .addParam('feedbaseAddress', 'feedbase address')
+    .setAction(async (args: TaskArguments, hre: HardhatRuntimeEnvironment) => {
+        const BasicReceiverFactoryDeployer = await hre.ethers.getContractFactory('BasicReceiverFactory')
+        const brf = await BasicReceiverFactoryDeployer.deploy(args.feedbaseAddress)
+        await brf.deployed()
+        console.log('BasicReceiverFactory deployed to : ', brf.address)
+    })
 
-    const BasicReceiverArtifact = await hre.artifacts.readArtifact('BasicReceiver')
-    const BasicReceiverFactoryArtifact = await hre.artifacts.readArtifact('BasicReceiverFactory')
-  })
-*/
+task('deploy-medianizer', 'deploy Medianizer')
+    .addParam('feedbaseAddress', 'feedbase address')
+    .setAction(async (args: TaskArguments, hre: HardhatRuntimeEnvironment) => {
+        const MedianizerFactory = await hre.ethers.getContractFactory('Medianizer')
+        const medianizer = await MedianizerFactory.deploy(args.feedbaseAddress)
+        await medianizer.deployed()
+
+        console.log('Medianizer deployed to : ', medianizer.address)
+    })
