cb02d7353ba2a6ff4dbfb966804afb158604f41a
Nikolai Mushegian <mail@nikolai.fyi>
2022-09-14 09:05:50 -0400

    simplify receiver

--- src/Medianizer.sol
+++ src/Medianizer.sol
@@ -5,66 +5,66 @@ pragma solidity ^0.8.15;
 import './Feedbase.sol';
 
 contract Medianizer {
-  address   public owner;
-  uint256   public quorum;
-  address[] public sources;
-  Feedbase  public feedbase;
+    address   public owner;
+    uint256   public quorum;
+    address[] public sources;
+    Feedbase  public feedbase;
 
-  constructor(address fb) {
-    owner = msg.sender;
-    feedbase = Feedbase(fb);
-  }
-
-  function setOwner(address newOwner) public {
-    require(msg.sender == owner, 'ERR_OWNER');
-    owner = newOwner;
-  }
-
-  function setSources(address[] calldata newSources) public {
-    require(msg.sender == owner, 'ERR_OWNER');
-    sources = newSources;
-  }
-
-  function setQuorum(uint newQuorum) public {
-    require(msg.sender == owner, 'ERR_OWNER');
-    quorum = newQuorum;
-  }
-
-  function poke(bytes32 tag) public {
-    bytes32[] memory data = new bytes32[](sources.length);
-    uint256 minttl = type(uint256).max;
-    uint256 count = 0;
-
-    for(uint256 i = 0; i < sources.length; i++) {
-      (bytes32 val, uint256 _ttl) = feedbase.pull(sources[i], tag);
-      if (block.timestamp > _ttl) {
-        continue;
-      }
-      if (count == 0 || val >= data[count - 1]) {
-        data[count] = val;
-      } else {
-        uint256 j = 0;
-        while (val >= data[j]) {
-          j++;
-        }
-        for(uint256 k = count; k > j; k--) {
-          data[k] = data[k - 1];
-        }
-        data[j] = val;
-      }
-      count++;
-    }
-    require(count > 0, 'ERR_COUNT');
-
-    bytes32 median;
-    if (count % 2 == 0) {
-      uint256 val1 = uint256(data[(count / 2) - 1]);
-      uint256 val2 = uint256(data[count / 2]);
-      median = bytes32((val1 + val2) / 2);
-    } else {
-      median = data[(count - 1) / 2];
+    constructor(address fb) {
+        owner = msg.sender;
+        feedbase = Feedbase(fb);
     }
 
-    feedbase.push(tag, median, minttl);
-  }
+    function setOwner(address newOwner) public {
+        require(msg.sender == owner, 'ERR_OWNER');
+        owner = newOwner;
+    }
+
+    function setSources(address[] calldata newSources) public {
+        require(msg.sender == owner, 'ERR_OWNER');
+        sources = newSources;
+    }
+
+    function setQuorum(uint newQuorum) public {
+        require(msg.sender == owner, 'ERR_OWNER');
+        quorum = newQuorum;
+    }
+
+    function poke(bytes32 tag) public {
+        bytes32[] memory data = new bytes32[](sources.length);
+        uint256 minttl = type(uint256).max;
+        uint256 count = 0;
+
+        for(uint256 i = 0; i < sources.length; i++) {
+            (bytes32 val, uint256 _ttl) = feedbase.pull(sources[i], tag);
+            if (block.timestamp > _ttl) {
+                continue;
+            }
+            if (count == 0 || val >= data[count - 1]) {
+                data[count] = val;
+            } else {
+                uint256 j = 0;
+                while (val >= data[j]) {
+                    j++;
+                }
+                for(uint256 k = count; k > j; k--) {
+                    data[k] = data[k - 1];
+                }
+                data[j] = val;
+            }
+            count++;
+        }
+        require(count > 0, 'ERR_COUNT');
+
+        bytes32 median;
+        if (count % 2 == 0) {
+            uint256 val1 = uint256(data[(count / 2) - 1]);
+            uint256 val2 = uint256(data[count / 2]);
+            median = bytes32((val1 + val2) / 2);
+        } else {
+            median = data[(count - 1) / 2];
+        }
+
+        feedbase.push(tag, median, minttl);
+    }
 }
--- src/Receiver.sol
+++ src/Receiver.sol
@@ -5,120 +5,110 @@ pragma solidity ^0.8.15;
 import './Feedbase.sol';
 
 contract BasicReceiverFactory {
-  Feedbase public feedbase;
-  mapping(address=>bool) public builtHere;
+    Feedbase public feedbase;
+    mapping(address=>bool) public built;
 
-  event BuiltBasicReceiver(address indexed receiver);
+    event Built(address indexed caller, address indexed receiver);
 
-  constructor(Feedbase fb) {
-    feedbase = fb;
-  }
+    constructor(Feedbase fb) {
+        feedbase = fb;
+    }
 
-  function build() public returns (BasicReceiver) {
-    BasicReceiver o = new BasicReceiver(feedbase);
-    builtHere[address(o)] = true;
-    emit BuiltBasicReceiver(address(o));
-    o.setOwner(msg.sender);
-    return o;
-  }
+    function build() public returns (BasicReceiver) {
+        BasicReceiver o = new BasicReceiver(feedbase);
+        built[address(o)] = true;
+        emit Built(msg.sender, address(o));
+        o.setOwner(msg.sender);
+        return o;
+    }
 }
 
 contract BasicReceiver {
-  Feedbase                 public feedbase;
-  address                  public owner;
-  mapping(address=>uint)   public signerTTL; // isSigner
-  mapping(address=>uint)   public signerSeq;
+    Feedbase                 public feedbase;
+    address                  public owner;
+    mapping(address=>bool)   public isSigner;
+    mapping(address=>uint)   public signerSeq;
 
-  event OwnerUpdate(address indexed oldOwner, address indexed newOwner);
-  event SignerUpdate(address indexed signer, uint signerTTL);
+    event OwnerUpdate(address indexed oldOwner, address indexed newOwner);
+    event SignerUpdate(address indexed signer, bool isSigner);
 
-  event Submit(
-      address indexed relayer
-    , address indexed signer
-    , bytes32 indexed tag
-    , uint256 indexed seq
-    , uint256         sec
-    , uint256         ttl
-    , bytes32         val
-  ) anonymous;
+    event Submit(
+        address indexed relayer
+      , address indexed signer
+      , bytes32 indexed tag
+      , uint256 indexed seq
+      , uint256         ttl
+      , bytes32         val
+    ) anonymous;
 
-  // bytes32 public constant SUBMIT_TYPEHASH = keccak256("Submit(bytes32 tag,uint256 seq,uint256 sec,uint256 ttl,bytes32 val)");
-  bytes32 public constant SUBMIT_TYPEHASH = 0x704ca89a84579f1c77f8af3ba18d619ac3bfe3ef4b477dd428170b1a3984c5d0;
-  bytes32 public immutable DOMAIN_SEPARATOR;
+    bytes32 public constant SUBMIT_TYPEHASH
+      = keccak256("Submit(bytes32 tag,uint256 seq,uint256 ttl,bytes32 val)");
+    bytes32 public immutable DOMAIN_SEPARATOR;
 
-  modifier auth {
-    require(msg.sender == owner, 'receiver-bad-owner');
-    _;
+    modifier auth {
+        require(msg.sender == owner, 'receiver-bad-owner');
+        _;
+    }
+
+    constructor(Feedbase fb) {
+        feedbase = fb;
+        owner = msg.sender;
+
+        // EIP712
+        DOMAIN_SEPARATOR = keccak256(abi.encode(
+            keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
+            keccak256("FeedbaseBasicReceiver"),
+            keccak256("1"),
+            chainId(),
+            address(this)
+       ));
+    }
+
+    function chainId() public view returns (uint256) {
+        return block.chainid;
+    }
+
+    // EIP712 digest
+    function digest(bytes32 tag, uint seq, uint ttl, bytes32 val) public view returns (bytes32) {
+        string memory header = "\x19Ethereum Signed Message:\n32";
+        bytes32 sighash = keccak256(abi.encodePacked(header,
+          keccak256(abi.encodePacked(
+            "\x19\x01", DOMAIN_SEPARATOR,
+            keccak256(abi.encode( SUBMIT_TYPEHASH, tag, seq, ttl, val ))
+          ))
+        ));
+        return sighash;
   }
 
-  constructor(Feedbase fb) {
-    feedbase = fb;
-    owner = msg.sender;
+    function submit(
+        bytes32 tag,
+        uint256 seq,
+        uint256 ttl,
+        bytes32 val,
+        uint8 v, bytes32 r, bytes32 s
+    ) public
+    {
+        bytes32 sighash = digest(tag, seq, ttl, val);
+        address signer = ecrecover(sighash, v, r, s);
 
-    // EIP712
-    DOMAIN_SEPARATOR = keccak256(abi.encode(
-      keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
-      keccak256("FeedbaseBasicReceiver"),
-      keccak256("1"),
-      chainId(),
-      address(this)
-    ));
-  }
+        require(isSigner[signer], 'receiver-submit-bad-signer');
+        require(block.timestamp <  ttl, 'receiver-submit-ttl');
+        require(seq > signerSeq[signer], 'receiver-submit-seq');
 
-  function chainId() public view returns (uint256) {
-    return block.chainid;
-  }
+        signerSeq[signer] = seq;
 
-  // EIP712 digest
-  function digest(bytes32 tag, uint seq, uint sec, uint ttl, bytes32 val) public view returns (bytes32) {
-    string memory header = "\x19Ethereum Signed Message:\n32";
-    bytes32 sighash = keccak256(abi.encodePacked(header, 
-      keccak256(abi.encodePacked(
-        "\x19\x01", DOMAIN_SEPARATOR,
-        keccak256(abi.encode(
-          SUBMIT_TYPEHASH, tag, seq, sec, ttl, val
-        ))
-      ))
-    ));
-    return sighash;
-  }
+        emit Submit(msg.sender, signer, tag, seq, ttl, val);
 
-  function submit(
-    bytes32 tag,
-    uint256 seq,
-    uint256 sec,
-    uint256 ttl,
-    bytes32 val,
-    uint8 v, bytes32 r, bytes32 s
-  ) public
-  {
-    bytes32 sighash = digest(tag, seq, sec, ttl, val);
-    address signer = ecrecover(sighash, v, r, s);
+        feedbase.push(tag, val, ttl);
+    }
 
-    uint sttl = signerTTL[signer];
-    require(block.timestamp < sttl, 'receiver-submit-bad-signer');
+    function setOwner(address newOwner) public auth {
+        emit OwnerUpdate(owner, newOwner);
+        owner = newOwner;
+    }
 
-    require(seq > signerSeq[signer], 'receiver-submit-seq');
-    signerSeq[signer] = seq;
-    require(block.timestamp >= sec, 'receiver-submit-sec');
-    require(block.timestamp <  ttl, 'receiver-submit-ttl');
-
-    emit Submit(msg.sender, signer, tag, seq, sec, ttl, val);
-
-    feedbase.push(tag, val, ttl);
-  }
-
-  function setOwner(address newOwner) public auth {
-    emit OwnerUpdate(owner, newOwner);
-    owner = newOwner;
-  }
-
-  function setSigner(address who, uint ttl) public auth {
-    signerTTL[who] = ttl;
-  }
-
-  function isSigner(address who) public view returns (bool) {
-    return block.timestamp < signerTTL[who];
-  }
+    function setSigner(address who, bool what) public auth {
+        isSigner[who] = what;
+    }
 
 }
