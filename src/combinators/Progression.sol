cfd9bc52ddb3a9553dfbea81d01d268ee91d5d8b
kbrav <>
2023-02-05 22:04:27 -0500

    valid

--- src/combinators/Progression.sol
+++ src/combinators/Progression.sol
@@ -18,6 +18,7 @@ contract Progression is Ward {
 
     mapping(bytes32=>uint) cachea;
     mapping(bytes32=>uint) cacheb;
+    mapping(bytes32=>bool) valid;
 
     mapping(bytes32=>Config) public configs;
     Feedbase public immutable fb;
@@ -29,10 +30,6 @@ contract Progression is Ward {
 
     function setConfig(bytes32 tag, Config calldata _config) public _ward_ {
         configs[tag] = _config;
-        (bytes32 price,) = fb.pull(_config.srca, _config.taga);
-        cachea[tag] = uint(price);
-        (price,) = fb.pull(_config.srcb, _config.tagb);
-        cacheb[tag] = uint(price);
     }
 
     // smooth progression
@@ -43,24 +40,25 @@ contract Progression is Ward {
         if (point > stretch) {
             point = stretch;
         }
+        (bytes32 pricea, uint ttla) = fb.pull(config.srca, config.taga);
+        (bytes32 priceb, uint ttlb) = fb.pull(config.srcb, config.tagb);
+        uint price = (uint(pricea) * (stretch - point) + uint(priceb) * point) / stretch;
 
         // later need to multiply the result by last / prog
-        uint prog = (cachea[tag] * (stretch - point) + cacheb[tag] * point) / stretch;
-        require(cachea[tag] > 0 && cacheb[tag] > 0, 'not initialized');
+        if (valid[tag]) {
+            uint prog = (cachea[tag] * (stretch - point) + cacheb[tag] * point) / stretch;
+            require(cachea[tag] > 0 && cacheb[tag] > 0, 'not initialized');
 
-        (bytes32 _pricea, uint ttla) = fb.pull(config.srca, config.taga);
-        uint pricea = uint(_pricea);
-        cachea[tag] = pricea;
-        (bytes32 _priceb, uint ttlb) = fb.pull(config.srcb, config.tagb);
-        uint priceb = uint(_priceb);
-        cacheb[tag] = priceb;
-
-        uint price = (pricea * (stretch - point) + priceb * point) / stretch;
-        (bytes32 _last,) = fb.pull(address(this), tag);
-        uint last = uint(_last);
-        if (last > 0) {
-            price = price * last / prog;
+            (bytes32 _last,) = fb.pull(address(this), tag);
+            uint last = uint(_last);
+            if (last > 0) {
+                price = price * last / prog;
+            }
+        } else {
+            valid[tag] = true;
         }
+        cachea[tag] = uint(pricea);
+        cacheb[tag] = uint(priceb);
         fb.push(tag, bytes32(price), ttla < ttlb ? ttla : ttlb);
     }
 }
