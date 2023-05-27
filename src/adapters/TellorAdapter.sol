9d4acecd264cede3ebe389f8d2da7a156387e951
kbrav <>
2023-05-27 03:01:40 -0400

    tellor adapter

---
+++ src/adapters/TellorAdapter.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+import { Feedbase } from '../Feedbase.sol';
+import { Ward } from '../mixin/ward.sol';
+
+interface ITellor {
+    function getNewValueCountbyRequestId(uint requestId) external view returns (uint);
+    function getTimestampbyRequestIDandIndex(uint requestId, uint idx) external view returns (uint);
+    function retrieveData(uint requestId, uint timestamp) external view returns (uint);
+}
+
+contract TellorAdapter is Ward {
+    ITellor public immutable tellor;
+    Feedbase public immutable fb;
+    struct Config {
+        uint reqid;
+        uint ttl;
+    }
+    mapping(bytes32 tag=>Config config) public configs;
+    error ErrNoData();
+
+    constructor (Feedbase _fb, address _tellor) {
+        tellor = ITellor(_tellor);
+        fb = _fb;
+    }
+
+    function setConfig(bytes32 tag, Config calldata _config) _ward_ public {
+        configs[tag] = _config;
+    }
+
+    function look(bytes32 tag) public {
+        Config storage config = configs[tag];
+        uint reqid = config.reqid;
+        uint256 count = tellor.getNewValueCountbyRequestId(reqid);
+        uint256 time = tellor.getTimestampbyRequestIDandIndex(reqid, count - 1);
+        uint256 value = tellor.retrieveData(reqid, time);
+        if (value > 0) fb.push(tag, bytes32(value), time + config.ttl);
+        else revert ErrNoData();
+    }
+}
