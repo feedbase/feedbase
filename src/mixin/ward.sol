0685e79f55917759221d9921f48d17063e8f4e35
kbrav <>
2023-06-05 22:08:38 -0400

    update solc version

--- src/mixin/ward.sol
+++ src/mixin/ward.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.20;
 
 contract Ward {
     event SetWard(address indexed caller, address indexed trusts, bool bit);
