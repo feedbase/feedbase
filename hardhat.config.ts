37ae9701971256359b03d40a70c24e77313f9d2d
kbrav <3114369+kbrav@users.noreply.github.com>
2023-06-10 07:11:52 -0400

    Revert "update solc version (#119)" (#121)
    This reverts commit e411eac0f873ea76366cd7fc9c10fd644561e2aa.
    
    Co-authored-by: kbrav <>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,7 +8,7 @@ export default {
   solidity: {
     compilers: [
         {
-            version: "0.8.20",
+            version: "0.8.19",
             settings: {
               optimizer: {
                 enabled: true,
--- src/mixin/ward.sol
+++ src/mixin/ward.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.19;
 
 contract Ward {
     event SetWard(address indexed caller, address indexed trusts, bool bit);
